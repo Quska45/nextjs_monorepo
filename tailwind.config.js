b524f46e432431563a8f19e459e7f7eede2540b6
GwangJin Byeon <qusrhkdwls45@gmail.com>
2024-08-16 23:56:05 +0900

    storybook에 tailwind 적용

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./packages/ui/components/**/*.tsx",
+    "./packages/ui/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
